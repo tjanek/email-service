c00e8be4333bbf0dc4bfb51674e7c769557177a9
Tomasz Janek <janek.tomasz@gmail.com>
2017-10-15 10:33:38 +0200

    Initial setup for Spring Boot and Gradle

---
+++ src/test/groovy/pl/tjanek/email/EmailServiceApplicationTests.groovy
@@ -0,0 +1,9 @@
+package pl.tjanek.email
+
+import org.springframework.boot.test.context.SpringBootTest
+import spock.lang.Specification
+
+@SpringBootTest
+class EmailServiceApplicationTests extends Specification {
+
+}
